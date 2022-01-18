4f3e8d5e4dcd33d906ce78a49ffa65b00d1a8640
Vitaly Levochka <levchik8383@gmail.com>
2022-01-18 11:58:57 +0200

    Express Router after fixing some problems

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use('/subdir', express.static(path.join(__dirname, '/public')));
 // routes
 app.use('/', require('./routes/root'));
 app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
     res.status(404);
